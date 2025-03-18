49865b2df2fc531137b9dd6b2c69bfa7b688f359
“antoineeripret” <antoine.eripret@gmail.com>
2025-03-18 08:17:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
         'google-auth>=2.13.0',
         'google-auth-oauthlib>=0.2.0',
         'google.cloud==0.34.0',
-        'pandas==2.2.0', 
+        'pandas==2.2.2', 
         'pandas_gbq>=0.22.0',  
         'validators==0.23.2',
         'tqdm==4.66.1',
         'prophet==1.1.5',
         'pycausalimpact==0.1.1',
         'numpy==1.26.3',
-        'requests==2.31.0'  
+        'requests==2.32.3'  
     ], 
     # Additional metadata about your package.
     author='Antoine Eripret',
